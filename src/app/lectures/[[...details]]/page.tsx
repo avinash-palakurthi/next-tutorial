e20b43206a620f0aef7d4d5217695fb61ca9c924
Avinash <palakurthiavinash07@gmail.com>
2024-05-04 19:54:51 +0530

    catch all segments in next-js

---
+++ src/app/lectures/[[...details]]/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function lectureDetails({ params }: { params: { details: string[] } }) {
+	console.log(params);
+	if (params.details?.length == 2) {
+		return <h3>lecture details 2</h3>;
+	} else if (params.details?.length == 3) {
+		return <h3>lecture details 3</h3>;
+	} else return <div>lectureDetails page</div>;
+}
+
+export default lectureDetails;
